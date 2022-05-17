ae8e1dd7fc53e91e0b83dc04414622a1d43db92e
CainanMCM <cacainan135@gmail.com>
2022-05-17 00:09:40 -0300

    iniciando os trabalhos

---
+++ src/translate/languages/pt.jsx
@@ -0,0 +1,19 @@
+const messages = {
+    pt: {
+        translations: {
+            titles: {
+                app: "Astronauta Marinho"
+            },
+            messages: {
+                sinopse: "Imersão em diversas sensibilidades, construindo climas grandiosos, minimalistas e comtemplativos.",
+                smallText: "Música: Mar Muleres"
+            },
+            buttons: {
+                save: "Ouvir",
+                lng: "Selecione um idioma"
+            }
+        }
+    }
+}
+
+export { messages }
